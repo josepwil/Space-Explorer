fdaebcdebe6516ea07d04ccf27bd03234281b397
Joseph Williams <josephwilliamsjs@gmail.com>
2021-03-20 15:35:43 -0400

    setting up Apollo Client

---
+++ start/client/src/index.tsx
@@ -0,0 +1,12 @@
+import {
+  ApolloClient,
+  NormalizedCacheObject
+} from '@apollo/client';
+import { cache } from './cache';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: 'http://localhost:4000/graphql'
+});
+
+
